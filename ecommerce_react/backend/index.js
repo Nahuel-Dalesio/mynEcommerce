df2ad41cb7f9ca4c4b94a27182176bb515a834c5
Nahue <nahuee.dalesiio@gmail.com>
2026-02-08 19:32:55 -0300

    cambio todo minuscula

--- ecommerce_react/backend/index.js
+++ ecommerce_react/backend/index.js
@@ -5,7 +5,7 @@ import productosRoutes from "./routes/productos.js";
 import { fileURLToPath } from "url";
 import pedidosRoutes from "./routes/pedidos.js";
 import dotenv from "dotenv";
-import './initDb.js';
+import './initdb.js';
 
 dotenv.config();
 
