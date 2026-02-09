387ec29a11f54a3b548e6aaf7460cfa20bb125cb
Nahue <nahuee.dalesiio@gmail.com>
2026-02-09 20:22:16 -0300

    cambio en conexion

--- ecommerce_react/backend/conexion.js
+++ ecommerce_react/backend/conexion.js
@@ -1,11 +1,7 @@
 import mysql from "mysql2/promise";
 
-  const pool = await mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+const pool = await mysql.createPool({
+  uri: process.env.DATABASE_URL,
 
   waitForConnections: true,
   connectionLimit: 10,
