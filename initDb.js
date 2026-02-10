7eb5090c17f6c9ca4c6ebe45e5b11c9c480b2813
Nahue <nahuee.dalesiio@gmail.com>
2026-02-10 11:31:31 -0300

    agrege bd y borre el json de railway

---
+++ initDb.js
@@ -0,0 +1,20 @@
+import fs from "fs";
+import pool from "./db.js"; // tu conexión MySQL
+
+async function initDb() {
+  try {
+    // Leer el archivo SQL completo
+    const sql = fs.readFileSync("init.sql", "utf8");
+
+    // Ejecutar el SQL
+    await pool.query(sql);
+
+    console.log("✅ Tablas creadas y datos insertados correctamente");
+    process.exit(0);
+  } catch (err) {
+    console.error("❌ Error ejecutando init.sql:", err);
+    process.exit(1);
+  }
+}
+
+initDb();
