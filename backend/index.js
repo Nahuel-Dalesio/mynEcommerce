feeb456df9fd0348be18584eca2029cd55c19ce9
Nahue <nahuee.dalesiio@gmail.com>
2026-02-10 06:38:13 -0300

    moviendo backend

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+import productosRoutes from "./routes/productos.js";
+import { fileURLToPath } from "url";
+import pedidosRoutes from "./routes/pedidos.js";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+app.use(
+  "/productos",
+  express.static(path.join(__dirname, "../frontend/public/productos")),
+);
+
+// rutas API
+app.use("/api/productos", productosRoutes);
+app.use("/api/pedidos", pedidosRoutes);
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto :${PORT}`);
+});
