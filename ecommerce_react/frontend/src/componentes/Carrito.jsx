daedb254d7193ca62e0ec98aff1521bfee8f1cc7
Nahue <nahuee.dalesiio@gmail.com>
2026-02-16 18:53:31 -0300

    detalles de responsive

--- ecommerce_react/frontend/src/componentes/Carrito.jsx
+++ ecommerce_react/frontend/src/componentes/Carrito.jsx
@@ -1,7 +1,8 @@
-import './Carrito.css';
-import { useEffect, useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Swal from 'sweetalert2';
+import "./Carrito.css";
+import { useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import IconClose from "../assets/iconClose.svg?react";
 
 function Carrito({
   abierto,
@@ -34,58 +35,63 @@ function Carrito({
     }
 
     function handleKeyDown(e) {
-      if (e.key === 'Escape' && abierto) cerrarCarrito();
+      if (e.key === "Escape" && abierto) cerrarCarrito();
     }
 
-    document.addEventListener('mousedown', handleClickFuera);
-    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener("mousedown", handleClickFuera);
+    document.addEventListener("keydown", handleKeyDown);
 
     return () => {
-      document.removeEventListener('mousedown', handleClickFuera);
-      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener("mousedown", handleClickFuera);
+      document.removeEventListener("keydown", handleKeyDown);
     };
   }, [abierto, cerrarCarrito, toggleButtonRef]);
 
   function irACheckout() {
     if (carrito.length === 0) {
       Swal.fire({
-        title: 'El carrito está vacío',
-        icon: 'warning',
+        title: "El carrito está vacío",
+        icon: "warning",
       });
       return;
     }
     Swal.fire({
-    title: "Antes de continuar...",
-    text: "Solo necesitamos algunos datos mínimos para completar tu pedido (nombre, apellido y teléfono).",
-    icon: "info",
-    showCancelButton: true,          // ← Muestra botón de cancelar
-    confirmButtonText: "Continuar",
-    cancelButtonText: "Volver", // ← Texto del botón de cancelar
-  }).then((result) => {
-    if (result.isConfirmed) {
-      cerrarCarrito();
-      navigate("/checkout");
-    }
-    // Si canceló, no hacemos nada, el carrito queda abierto
-  });
-
+      title: "Antes de continuar...",
+      text: "Solo necesitamos algunos datos mínimos para completar tu pedido (nombre, apellido y teléfono).",
+      icon: "info",
+      showCancelButton: true, // ← Muestra botón de cancelar
+      confirmButtonText: "Continuar",
+      cancelButtonText: "Volver", // ← Texto del botón de cancelar
+    }).then((result) => {
+      if (result.isConfirmed) {
+        cerrarCarrito();
+        navigate("/checkout");
+      }
+      // Si canceló, no hacemos nada, el carrito queda abierto
+    });
   }
   if (!abierto) return null;
 
   return (
-    <div className='mostrar' ref={carritoRef}>
-      <h2 className='tituloCarrito'>Carrito de compras</h2>
+    <div className="mostrar" ref={carritoRef}>
+      
+      <IconClose className="iconClose" onClick={cerrarCarrito} />
+      
+      <h2 className="tituloCarrito">Carrito de compras</h2>
       {carrito.map((p) => (
-        <ul className='card-carrito' key={`${p.idProducto}-${p.talle}`}>
-          <li className='lista'>
-            <img className='imagenCarrito' src={p.imagen} alt={p.nombre} />
+        <ul className="card-carrito" key={`${p.idProducto}-${p.talle}`}>
+          
+          <li className="lista">
+            <img className="imagenCarrito" src={p.imagen} alt={p.nombre} />
           </li>
-          <li className='lista art'>{p.nombre}</li>
-          <li className='lista talle'>Talle: {p.talle}</li>
-          <li className='lista'>${p.precio}</li>
-          <li className='lista'>Cantidad: {p.cantidad}</li>
+          <li className="lista art">
+            {p.nombre} {p.talle}
+          </li>
+          {/* <li className='lista talle'> {p.talle}</li> */}
+          <li className="lista">${p.precio}</li>
+          <li className="lista">Cantidad: {p.cantidad}</li>
           <button
-            className='eliminar-carrito'
+            className="eliminar-carrito"
             onClick={() => eliminarDelCarrito(p.idProducto, p.talle)}
           >
             ❌
@@ -93,9 +99,9 @@ function Carrito({
         </ul>
       ))}
       <div>
-        <p className='pTota'>Total: ${totalCarrito}</p>
+        <p className="pTota">Total: ${totalCarrito}</p>
       </div>
-      <button className='btn-comprar' onClick={irACheckout}>
+      <button className="btn-comprar" onClick={irACheckout}>
         Hacer pedido
       </button>
     </div>
