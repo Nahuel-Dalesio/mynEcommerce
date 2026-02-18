9c10d69d015d9345a29cce6f520657dfb90030d6
Nahue <nahuee.dalesiio@gmail.com>
2026-02-18 20:20:16 -0300

    agrego activo a prodctos + prueba de crud

--- models/products.model.js
+++ models/products.model.js
@@ -1,11 +1,11 @@
 import  pool  from "../conexion.js";
 
 export const createProduct = async (product) => {
-  const { name, description, price, esDestacado, enOferta, precioOferta, categoria } = product;
+  const { nombre, descripcion, precio, esDestacado, enOferta, precioOferta, categoria } = product;
 
   const [result] = await pool.query(
-    "INSERT INTO products (name, description, price, esDestacado, enOferta, precioOferta, categoria) VALUES (?, ?, ?, ?, ?, ?, ?)",
-    [name, description, price, esDestacado, enOferta, precioOferta, categoria]
+    "INSERT INTO producto (nombre, descripcion, precio, esDestacado, enOferta, precioOferta, categoria) VALUES (?, ?, ?, ?, ?, ?, ?)",
+    [nombre, descripcion, precio, esDestacado, enOferta, precioOferta, categoria]
   );
 
   return result.insertId;
